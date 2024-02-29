0aec2c63ae02059ad890142fd895d5ddd7ca1f11
User <michaelm810129@gmail.com>
2024-02-29 17:37:19 +1030

    added bootstrap navbar instead of custom javascript navbar

--- projects.js
+++ projects.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuBtn = document.querySelector('.hamburger--menu');
-    var navUl = document.querySelector('.nav--ul');
 
-    menuBtn.addEventListener('click', function () {
-        navUl.classList.toggle('show');
-        menuBtn.classList.toggle('marginBtn');
-    });
     displayProjects();
-    displayFooter();
+    // displayFooter();
     // revealItems();
 });
 document.addEventListener("scroll", revealItems);
@@ -46,11 +40,14 @@ const projects = [
 
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
-    // console.log(rect)
+
+    var footer = document.querySelector("footer").offsetHeight;
+    // var navbar = document.querySelector("nav--container").offsetHeight;
+    console.log(footer)
     return (
-        rect.top >= 0 &&
+        rect.top >= 0  &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom <= (window.innerHeight - footer) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 
